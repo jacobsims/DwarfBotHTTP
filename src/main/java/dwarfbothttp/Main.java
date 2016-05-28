0e50d916158ace56d85c0cd2b301a78c7eb009a3
Jacob Sims <jacobsims@users.noreply.github.com>
2016-05-27 23:10:50 -0400

    Basic working file upload for now

--- src/main/java/dwarfbothttp/Main.java
+++ src/main/java/dwarfbothttp/Main.java
@@ -1,7 +1,26 @@
 package dwarfbothttp;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import javax.servlet.MultipartConfigElement;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.velocity.VelocityTemplateEngine;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Run worked.");
+		VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+
+		Spark.get("/", (request, response) -> {
+			HashMap<String, Object> model = new HashMap<String, Object>();
+			return new ModelAndView(model, "index.vm");
+		}, velocityTemplateEngine);
+		Spark.post("/upload", (request, response) -> {
+			request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement((String)null));
+			try (InputStream inputStream = request.raw().getPart("to_convert").getInputStream()) {
+				System.out.println(inputStream);
+			}
+			return "file uploaded";
+		});
 	}
 }
